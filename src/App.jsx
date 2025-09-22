8755603470265d4605fab260c606b87944778004
shridmishra <shridmishra00@gmail.com>
2025-09-22 15:45:35 +0530

    implemented hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-5xl text-indigo-600">Welcome to gta</h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   );
 };
